75d0ee9ecc98089d8e2c27500a8c152c455d8467
dltndms <onlysui00@gmail.com>
2022-05-28 12:23:49 +0900

    ㅎㅎ

---
+++ py06.py
@@ -0,0 +1,12 @@
+print('퀴즈)구몬')
+문제1 = input('문제1> 3+3=')
+문제2 = input('문제2> 5-2=')
+문제3 = input('문제3> 5*2=')
+문제4 = input('문제4> 10/2=')
+문제5 = input('문제5> 10-3*3=')
+
+print('1번은 {}'.format(문제1))
+print('2번은 {}'.format(문제2))
+print('3번은 {}'.format(문제3))
+print('4번은 {}'.format(문제4))
+print('5번은 {}'.format(문제5))
